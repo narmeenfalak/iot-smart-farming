1fe0b1cdc10c0850c2c92f0b84fdef2303562cc3
Hafsa-Murtaza <hafsa@DESKTOP-1KRVAVG.localdomain>
2022-05-07 22:21:26 +0700

    Pump_Code

---
+++ Week_7/pump_code.ino
@@ -0,0 +1,10 @@
+int relayPin = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(relayPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(relayPin, HIGH);
+}
